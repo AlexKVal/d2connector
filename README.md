d6c4daaa77455f83cb08b5e7b66be35407b02541
AlexKVal <alexkval@gmail.com>
2016-02-22 01:35:38 +0200

    package and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# d2connector
+
+WIP: connector service to a d2base.
+
+## License
+`d2connector` is licensed under the [MIT License](https://github.com/alexkval/d2connector/blob/master/LICENSE).
---
+++ src/d2main.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const db = require("odbc")()
+
+const odbcString = "DSN=D2Main.NET"
+
+try {
+  db.openSync(odbcString)
+} catch (e) {
+  return console.log(`${err.message}\odbc> ${odbcString}`)
+}
+console.log('opened')
+
+const dbClose = function dbClose() {
+  db.closeSync()
+  console.log('closed')
+}
+
+const select = function select(sql) {
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, result) => {
+      if (err) reject(`${err.message}\nsql> ${sql}`)
+      else resolve(result)
+    })
+  })
+}
+
+const printPosIDs = result => result.forEach(row => console.log(`PosID: ${row.PosID}`))
+select("select PosID from sPOSs").then(printPosIDs)
+
+const printShortNames = result => result.forEach(row => console.log(`ShortName: ${row.ShortName}`))
+select("select ShortName from frMenu").then(printShortNames)
+.then(dbClose)
+
+// -----------------------------
